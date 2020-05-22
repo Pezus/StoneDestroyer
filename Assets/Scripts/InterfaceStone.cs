1dafbd7219660b2bd51964a30cb79d829833070f
Fayez Awad <awadfayez@gmail.com>
2020-05-22 12:30:16 -0400

    Initial Commit
    Initial Commit
    Original file of old project

---
+++ Assets/Scripts/InterfaceStone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceStone : MonoBehaviour
+{
+    public Text textThrown;
+    public Text textDestroyed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textThrown.text = "Stones thrown: " + GameManager.currentNumberStonesThrown;
+        textDestroyed.text = "Stones destroyed: " + GameManager.currentNumberDestroyedStones;
+    }
+}
